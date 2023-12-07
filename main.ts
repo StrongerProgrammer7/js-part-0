e2555da1f187413f35984941311a7519130d1678
Abdyukov Zufar <swat55551@gmail.com>
2023-12-07 18:07:32 +0300

    Transalte methods: allItemsHaveTheSameType getTypesOfItems not big fix

--- main.ts
+++ main.ts
@@ -1,6 +1,7 @@
+type NameTestBlock = (name:string) => void;
+type TypeValueFunc = (value: unknown) => void;
 
-
-const testBlock = (name:string): void =>
+const testBlock: NameTestBlock = (name) =>
 {
     console.groupEnd();
     console.group(`# ${name}\n`);
@@ -28,9 +29,37 @@ const test = (whatWeTest:string, actualResult:unknown, expectedResult:unknown):v
     }
 };
 
-const getType = (value: unknown):string =>
+const getType: TypeValueFunc = (value) =>
 {
     return typeof value;
+};
+
+const getTypesOfItems = (arr:Array<unknown>):Array<string> =>
+{
+    const types:Array<string> = [];
+    for (let i = 0; i < arr.length; i++)
+    {
+        types.push(typeof arr[i]);
+    }
+    return types;
+
+};
+
+const allItemsHaveTheSameType = (arr:Array<unknown>): boolean =>
+{
+    if (arr && arr.length === 0)
+    {
+        return true;
+    }
+
+    for (let i = 1; i < arr.length; i++)
+    {
+        if ((typeof arr[i]) !== (typeof arr[0]))
+        {
+            return false;
+        }
+    }
+    return true;
 };
 
 // Tests
@@ -50,3 +79,24 @@ test(
 );
 test('Undefined', getType(undefined), 'undefined');
 test('Null', getType(null), 'object');
+
+
+testBlock('allItemsHaveTheSameType');
+
+test('All values are numbers', allItemsHaveTheSameType([11, 12, 13]), true);
+
+test('All values are strings', allItemsHaveTheSameType(['11', '12', '13']), true);
+
+test(
+    'All values are strings but wait',
+    allItemsHaveTheSameType(['11', new String('12'), '13']),
+    false
+);
+// Number / Any => NaN => Number
+test(
+    'Values like a number',
+    allItemsHaveTheSameType([123, 123 / Number('a'), 1 / 0]),
+    true
+);
+
+test('Values like an object', allItemsHaveTheSameType([{}]), true);
